423e39b6144a9157fc3f6f0d34ad86cb393154c4
Jacob Leon Lyerla <umasour@yahoo.com>
2019-02-12 22:23:08 -0700

    formatted this file for better readability

--- Project/project.js
+++ Project/project.js
@@ -1,38 +1,56 @@
 // first were going to need to import in mongoose or require
 const mongoose = require("mongoose");
+
 // set a new variable, that will be set to a new mongoose schema.
 const Project = new mongoose.Schema({
+
   // declare the modal variables.
   title: {
     type: String,
+
     required: true
   },
+
   description: String,
+
   webpage:{
 type:String,
+
 default:"This code is not deployed."
   },
+
   github: {
     type: String,
+
     required: true
   },
+
   rating:{
       type:Number,
+
       required:true
   },
+
   youtube: String,
+
   img: {
     type: String,
+
     default: "https://logos-download.com/wp-content/uploads/2016/09/React_logo_logotype_emblem.png"
   },
+
   addedOn: {
     type: Date,
+
     default: Date.now
   },
+
   technologies:{
     type:Array,
+
     required:true,
   },
+  
   index:Number
 
 });
