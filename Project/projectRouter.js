286bfee48a75e7883c4d9015dc7385b93a3ecd8d
Jacob Leon Lyerla <umasour@yahoo.com>
2019-02-12 22:23:13 -0700

    formatted this file for better readability

--- Project/projectRouter.js
+++ Project/projectRouter.js
@@ -3,17 +3,24 @@ const router = require('express').Router();
 
 const Project =require('./project');
 // add in route handlers for post and get
+
 router.get('/:id',(req,res)=>{
+
     const{id}=req.params
+
     Project
     .findById(id)
+
     .then(response=>{
         res.status(202).json(response);
+
  })
+
  .catch(err=>{
      res.status(500).json({errorMessage: "The Project information could not be retrieved."})
  })
 })
+
 router.get('/', (req,res)=>{
     Project
     .find()
@@ -21,21 +28,28 @@ router.get('/', (req,res)=>{
     .then(response=>{
         res.status(200).json(response)
     })
+
     .catch(err=>{
         res.status(500).json({error:err})
     })
+
 })
+
 router
 .post('/',(req,res)=>{
     const project = new Project(req.body)
+
     project
     .save()
+
     .then(response=>{
         res.status(201).json(response)
     })
+
     .catch(err=>{
         res.status(500).json({error:err})
     })
+
 })
 
 // export the router
