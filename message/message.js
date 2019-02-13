eca473e94d77ca4d6f8f8e265bc74dfa4e308447
Jacob Leon Lyerla <umasour@yahoo.com>
2019-02-12 22:23:47 -0700

    formatted this file for better readability

--- message/message.js
+++ message/message.js
@@ -2,13 +2,11 @@ const mongoose = require("mongoose");
 
 // we set a new mongoose schema and name is Message
 const Message = new mongoose.Schema({
-
   /* email is a String and if there is no text sent in it will show "None Given" in side of my data base,
    i did not make anything required on my schema, it's just for sending messages to me directly,
      */
 
   email: {
-
     type: String,
 
     default: "None Given"
@@ -39,7 +37,6 @@ const Message = new mongoose.Schema({
 
     default: Date.now
   }
-
 });
 
 // exporting the modal it will actually set Message to messages as a third param here, but that is all generated procedurally
